27420541fe9d0fc49665e77d15d5a83f5354354b
mjcart <mjcart@naver.com>
2021-07-12 21:59:52 +0900

    5554_c++_jmj

---
+++ 5554/5554_c++_jmj/백준5554.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[4];
+	for (int i = 0; i < 4; i++) {
+		cin >> a[i];
+	}
+	int b=0;
+	for (int i = 0; i < 4; i++) {
+		b += a[i];
+	}
+	int m = b / 60;
+	int s = b % 60;
+	cout << m << endl << s;
+
+
+
+}
+
