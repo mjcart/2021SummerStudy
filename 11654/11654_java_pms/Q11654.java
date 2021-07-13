c3ddf437c35054276439bc78153b208c2b894470
umilove98 <umilove98@kakao.com>
2021-07-13 17:14:45 +0900

    Create Q11654.java

---
+++ 11654/11654_java_pms/Q11654.java
@@ -0,0 +1,15 @@
+package q11654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q11654 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int i = bf.readLine().charAt(0);
+		System.out.println(i);
+	}
+
+}
