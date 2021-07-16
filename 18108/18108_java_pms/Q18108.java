8ea3e031765f1499e7c16e17737fe30afecd803a
umilove98 <umilove98@kakao.com>
2021-07-16 14:04:16 +0900

    18108_java_pms

---
+++ 18108/18108_java_pms/Q18108.java
@@ -0,0 +1,15 @@
+package q18108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q18108 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		System.out.println(n-543);
+	}
+
+}
