66c665406692a87af481a7ea2f3ba807b5bf988d
umilove98 <umilove98@kakao.com>
2021-07-29 16:13:23 +0900

    Update Q1343.java

--- 1343/1343_java_pms/Q1343.java
+++ 1343/1343_java_pms/Q1343.java
@@ -9,42 +9,41 @@ public class Q1343 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String st = br.readLine();
-		char[] input = st.toCharArray();
+		char[] input = st.toCharArray();	// 입력값을 배열에 저장
 		
 		int cnt = 0;
 		char now;
 		
-		for(int i  = 0; i < input.length; i++) {
+		for(int i  = 0; i < input.length; i++) {	// 입력값 배열에서 한 char 씩 순회
 			now = input[i];
-			if (now == 'X') {
-				cnt++;
+			if (now == 'X') {	// 현재 값이 X인 경우
+				cnt++;	// 지나온 X의 개수를 누적시키는 변수
 			}
-			if (now == '.') {
+			if (now == '.') {	// 현재 값이 .인경우 그전까지 누적된 X의 개수가 홀수면 -1 찍고 종료 짝수인 경우 process 함수에 돌림 
 				if(cnt % 2 != 0) {
 					System.out.println("-1");
 					return;
 				}
 				process(cnt);
 
-				output = output + ".";
+				output = output + ".";	// X의 값을 AAAA, BB로 대체하여 추가한 뒤에 현재 값인 . 도 출력 문자열에 추가시켜줌 
 				
-				cnt = 0;
+				cnt = 0;	// 지금까지 누적된 X를 AAAA, BB로 대체하여 추가하였으므로 cnt를 초기화해 새로 X 누적 시작 
 			}
-			if(i == input.length -1) {
-				int n = cnt;
+			if(i == input.length -1) {	// 현재 index위치가 입력값의 마지막인 경우 남아있는 cnt값만큼을 출력해야하므로 홀수인지 확인하고 process 함수에 돌림
 				
-				if(n % 2 != 0) {
+				if(cnt % 2 != 0) {
 					System.out.println("-1");
 					return;
 				}
-				process(n);
+				process(cnt);
 				cnt = 0;
 			}
 
 		}
 		System.out.println(output);
 	}
-	static void process(int n) {
+	static void process(int n) {	// 중간에 채워야하는 X의 개수가 주어지면 AAAA를 이용하여 출력 문자열에 최대한 채운 뒤 나머지 수만큼 BB를 이용하여 채움 
 
 		int a = n / 4;
 		int b = (n % 4) / 2;
