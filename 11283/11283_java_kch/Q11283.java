91d90c1969c2f13f15ef5e874537254563cd9143
kkch1012 <62089304+kkch1012@users.noreply.github.com>
2021-08-01 11:16:12 +0900

    이게 나야
    this is me

---
+++ 11283/11283_java_kch/Q11283.java
@@ -0,0 +1,13 @@
+package bakjoon;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Q11283 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a =sc.next();
+		
+		char result = a.charAt(0);
+		System.out.println(((int) result) - 44031);
+}
+}
---
+++ 5522/5522_java_kch/one.java
@@ -0,0 +1,13 @@
+package bakjoon;
+import java.util.*;
+
+public class one {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int result = 0;
+			
+		for(int i=0;i<5;i++) result +=sc.nextInt();
+		
+		System.out.println(result);
+	}
+}
